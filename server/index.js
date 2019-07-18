d5896d26cbc7d80a5b83696fa8e9cb1c5b9d5bb4
Alex <aleksandr.dwt@gmail.com>
2019-07-18 20:13:53 +0300

    BE: added serving static

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const TASKS_URL = '/api/tasks';
 
@@ -9,6 +10,13 @@ const TasksController = require('./controllers/TasksController');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
+const CLIENT_FILES_PATH = '../client/build';
+app.use(express.static(path.join(__dirname, CLIENT_FILES_PATH)));
+app.get(
+  /^\/(?!api).*/,
+  (req, res) => res.sendFile(path.join(__dirname, CLIENT_FILES_PATH, 'index.html'))
+);
 
 app.use(TASKS_URL, TasksController);
 
