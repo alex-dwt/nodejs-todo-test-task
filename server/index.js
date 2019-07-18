86edaab992c64577b6fd52ffc217488337705b49
Alex Lukashevich <aleksandr.dwt@gmail.com>
2019-07-18 16:44:10 +0300

    BE: added cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const TASKS_URL = '/api/tasks';
 
@@ -7,6 +8,7 @@ const TasksController = require('./controllers/TasksController');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use(TASKS_URL, TasksController);
 
