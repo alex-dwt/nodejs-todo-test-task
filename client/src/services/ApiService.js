44b0a4c8ab954346f36ef753bab999f647ca29a8
Alex <aleksandr.dwt@gmail.com>
2019-07-18 21:13:02 +0300

    FE: changed api url

--- client/src/services/ApiService.js
+++ client/src/services/ApiService.js
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
-const BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = '/api';
 const TASKS_URL = BASE_URL + '/tasks';
 
 export async function getTasksListRequest() {
