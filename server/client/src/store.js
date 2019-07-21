85d14e343e07168af0c1f374c113ef80db6489f5
Alex <aleksandr.dwt@gmail.com>
2019-07-21 20:34:32 +0300

    SSR: fixed initialState

--- server/client/src/store.js
+++ server/client/src/store.js
@@ -7,15 +7,21 @@ import thunk from 'redux-thunk';
 
 import TasksReducer from './reducers/TasksReducer';
 
-// Grab the state from a global variable injected into the server-generated HTML
-const preloadedState = window.__PRELOADED_STATE__;
-// Allow the passed state to be garbage-collected
-delete window.__PRELOADED_STATE__;
+let preloadedState = null;
+if (typeof window !== 'undefined') {
+  // Grab the state from a global variable injected into the server-generated HTML
+  preloadedState = window.__PRELOADED_STATE__;
+  // Allow the passed state to be garbage-collected
+  delete window.__PRELOADED_STATE__;
+}
 
-export default createStore(
-    combineReducers({
-        tasks: TasksReducer,
-    }),
-    preloadedState,
-    applyMiddleware(thunk)
-);
+const reducers = combineReducers({
+  tasks: TasksReducer,
+});
+const middleware = applyMiddleware(thunk);
+
+const store = preloadedState !== null
+  ? createStore(reducers, preloadedState, middleware)
+  : createStore(reducers, middleware);
+
+export default store;
